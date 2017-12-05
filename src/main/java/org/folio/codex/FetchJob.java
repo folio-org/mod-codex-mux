ba6a1f630f72ecc3d460e1d737f43956394bb806
Adam Dickmeiss <adam@indexdata.dk>
2017-12-05 16:36:48 +0100

    Add RoundRobin fetch util

---
+++ src/main/java/org/folio/codex/FetchJob.java
@@ -0,0 +1,44 @@
+package org.folio.codex;
+
+import java.util.List;
+
+public class FetchJob {
+
+  int totalRecords;
+  int offset;
+  int limit;
+
+  public FetchJob(int total) {
+    totalRecords = total;
+    offset = limit = -1;
+  }
+
+  public static void roundRobin(List<FetchJob> jobs, int offset, int limit) {
+    for (FetchJob job : jobs) {
+      job.limit = 0;
+    }
+    int fetched = 0;
+    int gPos = 0;
+    int jPos = 0;
+    boolean more = true;
+    while (more) {
+      more = false;
+      for (FetchJob job : jobs) {
+        if (gPos >= offset + limit) {
+          break;
+        }
+        if (jPos < job.totalRecords) {
+          if (gPos >= offset && gPos < offset + limit) {
+            if (job.limit == 0) {
+              job.offset = jPos;
+            }
+            job.limit++;
+          }
+          more = true;
+          gPos++;
+        }
+      }
+      jPos++;
+    }
+  }
+}
